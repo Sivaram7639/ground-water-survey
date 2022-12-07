0b8dd021e8948b40aec73103cf136190f2cb215c
Sivaram7639 <118120553+Sivaram7639@users.noreply.github.com>
2022-12-07 10:45:44 +0530

    Update Designer.js

--- src/components/Designer.js
+++ src/components/Designer.js
@@ -5,7 +5,7 @@ const Designer = () => {
   return (
     <div className="designerpage">
     <h1 className="copyright">© Copyright 2025 MPR LLC-All Rights Reserved</h1>
-    <p className="designer">Website designed by - <a className="designername" href="https://www.instagram.com/n.siva.16">SIVA.N</a> </p>
+    <p className="designer">Website designed by -</p><a className="designername" href="https://www.instagram.com/n.siva.16">SIVA.N</a> 
 
     </div>
   )
